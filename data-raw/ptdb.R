38bf88dfb7d82d00e25dcce33d5b6fe724a9b45d
trashbirdecology <jessicaleighburnett@gmail.com>
2020-08-26 13:00:12 -0600

    v. minor edits

--- data-raw/ptdb.R
+++ data-raw/ptdb.R
@@ -1,10 +1,11 @@
-# Load in the raw, very raw data (last upload 20200826)
-ptdb <- readr::read_csv(here::here("data-raw/parrot-traits-database.csv"), skip=1)
+## This script loads in and munges whatever the most recent or useful version of the PTDB is.
+## As of 20200826 the development versions of the database are offline.
 
+# Load in the database ----------------------------------------------------
+ptdb <- readr::read_csv(here::here("data-raw/parrot-traits-database.csv"), skip=1)
 
 # Munging -----------------------------------------------------------------
 
 
 # Update .rda data file ---------------------------------------------------
-
-usethis::use_data(ptdb)
+usethis::use_data(ptdb, overwrite = TRUE)
