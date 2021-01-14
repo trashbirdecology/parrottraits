d9ad3c1bcb1eb06fc35d16a31235bd190155e941
jburnett <jburnett@usgs.gov>
2021-01-14 11:17:52 -0700

    mostly documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+
+# traits ------------------------------------------------------------------
+#' Parrot Trait Database
+#'
+#' A comprehensive and curated dataset containing all known and credible traits for parrot species worldwide.
+#' Taxonomic information used to define and identify individual species is _____
+#'
+#' @format A data frame with 414 rows (species) and 180 variables (traits and taxonomic information:
+#' \describe{
+  # \item{price}{price, in US dollars}
+  # \item{carat}{weight of the diamond, in carats}
+  # ...
+#' }
+#' @usage data(traits)
+#' @source Kevin R. Burgio personal communication
+
+"traits"
---
+++ data-raw/traits.R
@@ -0,0 +1,11 @@
+## This script loads in and munges whatever the most recent or useful version of the PTDB is.
+## As of 20200826 the development versions of the database are offline.
+
+# Load in the original data ----------------------------------------------------
+# this data was obtained from KR Burgio in 2020-11
+traits <- readr::read_csv(here::here("data-raw/parrot-traits-database.csv"), skip=1)
+
+# Update .rda data file ---------------------------------------------------
+usethis::use_data(traits, overwrite = TRUE)
+
+
